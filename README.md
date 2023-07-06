fa95d0baf068b2f2b353465ebf93ad62ef16f765
zhangyu1818 <zhangyu1818@qq.com>
2023-07-06 12:49:34 +0800

    add script

---
+++ README.md
@@ -0,0 +1,4 @@
+# ts-challenges-answer
+
+
+  
---
+++ scripts/genReadme.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import { readdir, writeFile } from 'fs/promises'
+
+const cwd = process.cwd()
+
+const srcPath = path.resolve(cwd, './src')
+const readmePath = path.resolve(cwd, './README.md')
+
+;(async () => {
+  const files = await readdir(srcPath)
+  const content = `# ts-challenges-answer
+
+${files
+  .map(
+    (file) =>
+      `[${path.basename(
+        file,
+        '.ts',
+      )}](https://github.com/zhangyu1818/type-challenges-answer/blob/main/src/${file})`,
+  )
+  .join('\n')}
+  `
+  await writeFile(readmePath, content, 'utf-8')
+})()
